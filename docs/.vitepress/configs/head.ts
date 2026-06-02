1a6d7ae87a744d009b719566f3052ea48b94b84c
lining <2357490894@qq.com>
2026-06-02 14:51:03 +0800

    feat: 配置head，获取 GitHub 仓库名作为 base 路径

--- docs/.vitepress/configs/head.ts
+++ docs/.vitepress/configs/head.ts
@@ -1,13 +1,18 @@
 import type { HeadConfig } from 'vitepress'
+import { basename } from 'node:path'
+
+// 获取 GitHub 仓库名作为 base 路径
+const APP_BASE_PATH = basename(process.env.GITHUB_REPOSITORY || '')
+const BASE_URL = APP_BASE_PATH ? `/${APP_BASE_PATH}/` : '/'
 
 export const head: HeadConfig[] = [
-  ['link', { rel: 'icon', href: '/favicon.ico' }],
-  ['link', { rel: 'apple-touch-icon', href: '/favicon.ico' }],
-  ['link', { rel: 'mask-icon', href: '/favicon.ico', color: '#3eaf7c' }],
+  ['link', { rel: 'icon', href: `${BASE_URL}favicon.ico` }],
+  ['link', { rel: 'apple-touch-icon', href: `${BASE_URL}favicon.ico` }],
+  ['link', { rel: 'mask-icon', href: `${BASE_URL}favicon.ico`, color: '#3eaf7c' }],
   ['meta', { name: 'theme-color', content: '#3eaf7c' }],
   ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
   ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-  ['meta', { name: 'msapplication-TileImage', content: '/favicon.ico' }],
+  ['meta', { name: 'msapplication-TileImage', content: `${BASE_URL}favicon.ico` }],
   ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
-  ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
+  ['link', { rel: 'manifest', href: `${BASE_URL}manifest.webmanifest` }],
 ]
