8da430f1b3a5adac329b7410c9477ab20062074f
lining <2357490894@qq.com>
2026-06-02 13:29:56 +0800

    feat: 同上

--- docs/.vitepress/configs/head.ts
+++ docs/.vitepress/configs/head.ts
@@ -1,12 +1,12 @@
 import type { HeadConfig } from 'vitepress'
 
 export const head: HeadConfig[] = [
-  ['link', { link: 'icon', href: '/favicon.ico' }],
+  ['link', { link: 'icon', href: '/ning-notes/favicon.ico' }],
   ['meta', { name: 'theme-color', content: '#3eaf7c' }],
   ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
   ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
   ['meta', { name: 'msapplication-TileImage', content: '/favicon.ico' }],
   ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
-  ['link', { rel: 'apple-touch-icon', href: '/favicon.ico' }],
-  ['link', { rel: 'mask-icon', href: '/favicon.ico', color: '#3eaf7c' }],
+  ['link', { rel: 'apple-touch-icon', href: '/ning-notes/favicon.ico' }],
+  ['link', { rel: 'mask-icon', href: '/ning-notes/favicon.ico', color: '#3eaf7c' }],
 ]
